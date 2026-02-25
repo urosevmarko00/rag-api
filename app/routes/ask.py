fad7b76a4265b411731e97db97e0149268818763
Marko Urosev <urosev.marko00@gmail.com>
2026-02-25 13:48:50 +0100

    add regions for GET and POST

--- app/routes/ask.py
+++ app/routes/ask.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+router = APIRouter()
 
+
+# region POSTS
 class QuestionRequest(BaseModel):
     question: str
-
-
-router = APIRouter()
 
 
 @router.post("/ask")
 def ask_question(request: QuestionRequest):
     return {"received": request}
+# endregion
--- app/routes/health.py
+++ app/routes/health.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
+# region GETS
 @router.get("/health")
 def health():
     return {"status": "ok"}
+# endregion
