7ea77e76bb12f391fd3f7b9a93663ffdaa339e11
Marko Urosev <urosev.marko00@gmail.com>
2026-02-27 15:23:20 +0100

    Update RAGService with ask() method and add_document() method using vector store and embedding service

--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,16 +1,36 @@
+from app.models import models
+
+
 class RAGService:
 
-    def __init__(self):
+    def __init__(self, embedding_service, vector_store, chunking_service):
         self.model_name = "dummy_llm"
+        self.embedding = embedding_service
+        self.vector_store = vector_store
+        self.chunking = chunking_service
 
     def ask(self, question: str) -> dict:
         if not question.strip():
             raise ValueError("Question cannot be empty or whitespace")
-        context = "Fake context"
-        answer = f"[{self.model_name}] {question}"
+
+        query_embedding = self.embedding.embed(question)
+        results = self.vector_store.search(query_embedding)
+
+        if not results:
+            context = ""
+            answer = "No documents available yet."
+        else:
+            context = "\n".join([text for _, text in results])
+            answer = f"[{self.model_name}]\nContext:\n {context}\n\n Question:\n{question}"
 
         return {
             "question": question,
             "context": context,
             "answer": answer
         }
+
+    def add_document(self, document: models.Document):
+        chunks = self.chunking.chunk(document.content)
+
+        for chunk in chunks:
+            self.vector_store.add(chunk, self.embedding.embed(chunk))
